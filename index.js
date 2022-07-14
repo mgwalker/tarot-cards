81070441113524b4a25d789c175fa2cf65158b7d
Greg Walker <mgwalker@gmail.com>
2022-07-13 21:23:06 -0500

    initial

---
+++ index.js
@@ -0,0 +1,43 @@
+import { chromium } from "playwright";
+import download from "download";
+
+const config = { baseURL: "https://en.wikipedia.org/" };
+
+const browser = await chromium.launch();
+const page = await browser.newPage(config);
+await page.goto("https://en.wikipedia.org/wiki/Rider–Waite_tarot_deck");
+
+await page.waitForSelector("#Major_Arcana");
+
+const imagePageLinks = await page.evaluate(() => {
+  const img = document.querySelectorAll("ul.gallery div.thumb a.image");
+  return [...img].map((i) => i.getAttribute("href"));
+});
+
+const imageLinks = [];
+for await (const link of imagePageLinks) {
+  await page.goto(link);
+  await page.waitForSelector(
+    "#mw-content-text div.fullImageLink#file a:not([class])"
+  );
+  const imageLink = await page
+    .locator("#mw-content-text div.fullImageLink#file a:not([class])")
+    .getAttribute("href");
+
+  imageLinks.push(`https:${imageLink}`);
+}
+
+for await(const link of imageLinks) {
+  const filename = ["minor", link.split("/").pop().toLowerCase()]
+    .join("_")
+    .replace("minor_rws_tarot", "major")
+    .replace("pents", "pentacles")
+    .replace("minor_thelovers.jpg", "major_06_lovers.jpg")
+    .replace("minor_tarot_nine_of_wands.jpg", "minor_wands09.jpg")
+
+  console.log(link, filename);
+
+  await download(link, "imgs",{filename})
+}
+
+browser.close();
